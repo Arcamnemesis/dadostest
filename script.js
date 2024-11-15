3c952b187aa5e337ba7c1414b4f010bf40f9dd80
Arcamnemesis <146767588+Arcamnemesis@users.noreply.github.com>
2024-11-14 19:39:49 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botonRuleta = document.getElementById("botonRuleta");
-
-    botonRuleta.addEventListener("click", function() {
-        // Redirigir a una página de ruleta (sustituye el enlace por la URL de tu ruleta)
-        window.location.href = "https://www.ejemplo.com/ruleta";
-    });
-});
